b788368fc6fc8f7174f25c2add909f867fa31d0d
Bástyai Botond <64700017+botator@users.noreply.github.com>
2025-02-20 10:50:35 +0100

    lobster img

--- rakOs/rako/app.js
+++ rakOs/rako/app.js
@@ -252,3 +252,69 @@ async function openfolder() {
         });
     });
 }
+function lobster(){
+    let div = document.createElement('div');
+    let table = document.getElementById("table");
+    let img = document.createElement('img');
+    let fejlec = document.createElement('div');
+    fejlec.style = "height:50px;background-color:black";
+    div.draggable = "true";
+    img.src = "src/lobster.png";
+    div.classList.add("draggable");
+    div.style = "width:fit-content;height:fit-content;z-index:1001;";
+    div.appendChild(fejlec);
+    div.appendChild(img);
+    table.appendChild(div);
+    
+    document.querySelectorAll(".draggable").forEach(draggable => {
+        console.log(draggable);
+        draggable.addEventListener("mousedown", (event) => {
+            item1 = draggable;
+
+            let shiftX = event.clientX - draggable.getBoundingClientRect().left;
+            let shiftY = event.clientY - draggable.getBoundingClientRect().top;
+
+            const moveAt = (pageX, pageY) => {
+                let newX = pageX - shiftX;
+                let newY = pageY - shiftY;
+
+
+                if (newX < 0) newX = 0;
+                if (newY < 0) newY = 0;
+                if (newX + draggable.offsetWidth > container.offsetWidth) newX = container.offsetWidth - draggable.offsetWidth;
+                if (newY + draggable.offsetHeight > container.offsetHeight) newY = container.offsetHeight - draggable.offsetHeight;
+
+                draggable.style.left = newX + "px";
+                draggable.style.top = newY + "px";
+
+                draggable.style.zIndex = "999";
+
+                // console.log(draggable.id)
+            }
+
+            startPosX = draggable.style.left;
+            startPosY = draggable.style.top;
+
+            const onMouseMove = (event) => {
+                moveAt(event.pageX, event.pageY);
+                checkCollisionOnDrag(draggable); // Check collision while dragging
+            }
+
+            document.addEventListener("mousemove", onMouseMove);
+
+            draggable.addEventListener("mouseup", () => {
+                document.removeEventListener("mousemove", onMouseMove);
+                CheckDelete();
+                CheckCollide();
+                draggable.style.zIndex = "1";
+            });
+
+            draggable.ondragstart = () => {
+                return false;
+            };
+        });
+    });
+   
+    
+    
+}
