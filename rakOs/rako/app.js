7af7b0272695b24e8f51ad20d2f3cbb1d0988fe9
ORIGINALrak <90960081+ORIGINALrak@users.noreply.github.com>
2025-02-20 11:13:16 +0100

    txt, meg átláthatóbb javascript

--- rakOs/rako/app.js
+++ rakOs/rako/app.js
@@ -102,53 +102,7 @@ function newIcon() {
     div.appendChild(img);
     div.appendChild(input);
     table.appendChild(div);
-    document.querySelectorAll(".draggable").forEach(draggable => {
-        draggable.addEventListener("mousedown", (event) => {
-            item1 = draggable;
-
-            let shiftX = event.clientX - draggable.getBoundingClientRect().left;
-            let shiftY = event.clientY - draggable.getBoundingClientRect().top;
-
-            const moveAt = (pageX, pageY) => {
-                let newX = pageX - shiftX;
-                let newY = pageY - shiftY;
-
-
-                if (newX < 0) newX = 0;
-                if (newY < 0) newY = 0;
-                if (newX + draggable.offsetWidth > container.offsetWidth) newX = container.offsetWidth - draggable.offsetWidth;
-                if (newY + draggable.offsetHeight > container.offsetHeight) newY = container.offsetHeight - draggable.offsetHeight;
-
-                draggable.style.left = newX + "px";
-                draggable.style.top = newY + "px";
-
-                draggable.style.zIndex = "999";
-
-                // console.log(draggable.id)
-            }
-
-            startPosX = draggable.style.left;
-            startPosY = draggable.style.top;
-
-            const onMouseMove = (event) => {
-                moveAt(event.pageX, event.pageY);
-                checkCollisionOnDrag(draggable); // Check collision while dragging
-            }
-
-            document.addEventListener("mousemove", onMouseMove);
-
-            draggable.addEventListener("mouseup", () => {
-                document.removeEventListener("mousemove", onMouseMove);
-                CheckDelete();
-                CheckCollide();
-                draggable.style.zIndex = "1";
-            });
-
-            draggable.ondragstart = () => {
-                return false;
-            };
-        });
-    });
+    draggable()
 }
 
 async function openfolder() {
@@ -203,6 +157,10 @@ async function openfolder() {
     icondiv.appendChild(iconimg);
     tray.appendChild(icondiv);
 
+    draggable()
+}
+
+function draggable(){
     document.querySelectorAll(".draggable").forEach(draggable => {
         console.log(draggable);
         draggable.addEventListener("mousedown", (event) => {
@@ -251,4 +209,18 @@ async function openfolder() {
             };
         });
     });
+    document.querySelectorAll(".draggable").forEach(draggable => {
+        draggable.style.zIndex = "1";
+    });
+}
+function txt() {
+    console.log("txt");
+    let embed = document.createElement("embed");
+    embed.type = "text/html";
+    embed.src = "../txt/txt.html";
+    embed.width = "2000px";
+    embed.height = "1000px";
+    embed.style.paddingTop = "100px";
+    let tableElement = document.getElementById("table");
+    tableElement.appendChild(embed);
 }
